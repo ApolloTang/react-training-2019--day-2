af7b4760518c54a97781bfecb1de9af1ba7205c9
Apollo Tang <tang.apollo@gmail.com>
2019-11-29 02:21:02 -0500

    remove faulty description

--- setup-a-webpack/src/01-jest-mock-and-spying/01-example-to-use-mock/tests--my-module-consumer/04-automock.test.js
+++ setup-a-webpack/src/01-jest-mock-and-spying/01-example-to-use-mock/tests--my-module-consumer/04-automock.test.js
@@ -12,9 +12,7 @@ jest.mock('../my-module') // use auto mock for simple case
 //
 // *****************************************************
 
-describe('This will not work', ()=>{ // eslint-disable-line
   test('mockFunction has been called with correct text', () => {
     myModuleConsumer()
     expect(myModule).toHaveBeenCalledWith('it has been called in consumer')
   })
-})
